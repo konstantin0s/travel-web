5b5bd9739e5ecf3f98b02f90f63b00170a2fd851
konstantin0s <constantintofan85@gmail>
2018-04-16 19:52:28 +0200

    Add carousel & images

--- js/travel.js
+++ js/travel.js
@@ -1,2 +1,12 @@
+//Sidebar
 const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {});
+
+//Slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 6000
+});
